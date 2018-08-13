96dea95445ec5d6dca74f143723961795c692e2d
Dmitry Skorlupkin <daemych@gmail.com>
2018-08-13 21:06:40 +0300

    Base for future MVVM visualization

---
+++ Maze002/ViewModels/CellStyleVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+
+namespace Maze002.ViewModels
+{
+    public class CellStyleVM:ViewModelBase
+    {
+        double simpleSize;
+        SolidColorBrush brush;
+
+        public double SimpleSize { get => simpleSize; set => SetField(ref simpleSize, value); }
+        public string HexFill { get => brush.Color.ToString(); set => SetField(ref brush, new SolidColorBrush((Color)ColorConverter.ConvertFromString(value))); }
+    }
+}
